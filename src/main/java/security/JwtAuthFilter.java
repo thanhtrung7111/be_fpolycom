59ec718d6e265e2d2632a72f2d229ff78d36fa03
thanhtrung <trungdtps27866@fpt.edu.vn>
2024-09-30 17:25:25 +0700

    Commit

--- src/main/java/security/JwtAuthFilter.java
+++ src/main/java/security/JwtAuthFilter.java
@@ -6,17 +6,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import service.AdministrationService;
-import service.JWTService;
+import service.common.JWTService;
 import service.StoreService;
 import service.UserAccountService;
 
--- src/main/java/service/common/EncodingService.java
+++ src/main/java/service/common/EncodingService.java
@@ -1,4 +1,4 @@
-package service;
+package service.common;
 
 import org.springframework.stereotype.Component;
 
