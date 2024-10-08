535c6d65dc0c29659d14fdbbda903d1c3c0be198
thanhtrung <thanhtrung71119999@gmail.com>
2024-10-08 20:44:26 +0700

    Commit

--- src/main/java/dto/store_follow/StoreFollowRequestDTO.java
+++ src/main/java/dto/store_follow/StoreFollowRequestDTO.java
@@ -1,6 +1,7 @@
 package dto.store_follow;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ public class StoreFollowRequestDTO {
     @NotBlank(message = "Khong de trong userLogin!")
     String userLogin;
 
-    @NotBlank(message = "Khong de trong id cua hang!")
+    @NotNull(message = "Khong de trong id cua hang!")
     Long storeCode;
 
 
