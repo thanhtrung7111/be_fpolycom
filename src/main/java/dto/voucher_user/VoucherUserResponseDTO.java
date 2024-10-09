3ef2759654e62e0129ac9ef4916273a9f0b44ee4
thanhtrung <trungdtps27866@fpt.edu.vn>
2024-10-09 17:31:56 +0700

    Commit

---
+++ src/main/java/dto/voucher_user/VoucherUserResponseDTO.java
@@ -0,0 +1,11 @@
+package dto.voucher_user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoucherUserResponseDTO {
+}
