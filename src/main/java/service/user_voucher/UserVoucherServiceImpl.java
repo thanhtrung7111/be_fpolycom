8ee4389e049f7708f2842a5b91ef4720f35d36b7
thanhtrung <thanhtrung71119999@gmail.com>
2024-10-10 20:07:35 +0700

    Commit

---
+++ src/main/java/service/user_voucher/UserVoucherServiceImpl.java
@@ -0,0 +1,18 @@
+package service.user_voucher;
+
+import dto.voucher_user.VoucherUserRequestDTO;
+import dto.voucher_user.VoucherUserResponseDTO;
+
+import java.util.List;
+
+public class UserVoucherServiceImpl implements UserVoucherService{
+    @Override
+    public List<VoucherUserResponseDTO> getAllVoucherByUser(VoucherUserRequestDTO requestDTO) {
+        return null;
+    }
+
+    @Override
+    public VoucherUserResponseDTO postUserVoucher(VoucherUserRequestDTO requestDTO) {
+        return null;
+    }
+}
